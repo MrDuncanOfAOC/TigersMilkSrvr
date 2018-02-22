abbb0cdefb2c50ac546f600748801d3d6d6ec407
carlduncan <carl.e.duncan@gmail.com>
2018-02-22 16:43:04 -0600

    Futher enchanments of Auth process

--- TigersMilkSrvr/Models/User.cs
+++ TigersMilkSrvr/Models/User.cs
@@ -127,51 +127,58 @@ namespace TigersMilkSrvr.Models
             
             OracleConnection conn = new OracleConnection(ConfigurationManager.ConnectionStrings["aocport"].ToString());
             OracleCommand cmd = conn.CreateCommand();
-            User user;
+            User user = null;
 
             try
             {
                 conn.Open();
-                
-                string sql = "SELECT U.*, R.role_desc, UT.type_desc " +
-                             " FROM " +
-                             "   tm_users U, " + 
-                             "   tm_roles R, " +
-                             "   tm_user_type UT " +
-                             "WHERE(1 = 1) " +
-                             "      AND UPPER (user_name) = '" + username + "' " +
-                             "      AND password = '" + password + "' " +
-                             "      AND account_suspended <= 0 " +
-                             "      AND U.role = R.role_id " +
-                             "      AND R.user_type = UT.type_id";
+                cmd.CommandText = "(select AOCPORT.xxaoc_crypto_util_pkg.sf_authenticate_pwd_str((select upper(user_name) from TM_USERS where upper(email_address) = '" + username.ToUpper() + "'), '" + password + "') valid from dual)";
 
-
-                cmd.CommandText = sql;
-
-
-                OracleDataAdapter da = new OracleDataAdapter(cmd);
-                DataSet ds = new DataSet();
-
-                da.Fill(ds);
-
-                if (ds.Tables.Count > 0 && ds.Tables[0].Rows.Count > 0)
+                if (cmd.ExecuteScalar().ToString().Equals("TRUE", StringComparison.CurrentCultureIgnoreCase))
                 {
-                    DataRow  dr = ds.Tables[0].Rows[0];
-                    user = new User();
-                    user.UserId = int.Parse(dr["user_id"].ToString());
-                    user.UserName = dr["user_name"].ToString();
-                    user.FirstName = dr["first_name"].ToString();
-                    user.LastName = dr["last_name"].ToString();
-                    user.EmailAddress = dr["email_address"].ToString();
 
-                    Token zToken = new Token
+
+                    string sql = "SELECT U.*, R.role_desc, UT.type_desc " +
+                                 " FROM " +
+                                 "   tm_users U, " +
+                                 "   tm_roles R, " +
+                                 "   tm_user_type UT " +
+                                 "WHERE(1 = 1) " +
+                                 "      AND UPPER (email_address) = '" + username.ToUpper() + "' " +
+                                 //"      AND password = '" + password + "' " +
+                                 "      AND account_suspended <= 0 " +
+                                 "      AND U.role = R.role_id " +
+                                 "      AND R.user_type = UT.type_id";
+
+
+                    cmd.CommandText = sql;
+
+
+                    OracleDataAdapter da = new OracleDataAdapter(cmd);
+                    DataSet ds = new DataSet();
+
+                    da.Fill(ds);
+
+                    if (ds.Tables.Count > 0 && ds.Tables[0].Rows.Count > 0)
                     {
-                        userName = user.UserName,
-                        userId = user.UserId,
-                        issueDtm = DateTime.Now
-                    };
-                    user.Token = TokenManager.GenerateAuthToken(zToken);
+                        DataRow dr = ds.Tables[0].Rows[0];
+                        user = new User();
+                        user.UserId = int.Parse(dr["user_id"].ToString());
+                        user.UserName = dr["user_name"].ToString();
+                        user.FirstName = dr["first_name"].ToString();
+                        user.LastName = dr["last_name"].ToString();
+                        user.EmailAddress = dr["email_address"].ToString();
 
+                        Token zToken = new Token
+                        {
+                            userName = user.UserName,
+                            userId = user.UserId,
+                            issueDtm = DateTime.Now
+                        };
+                        user.Token = TokenManager.GenerateAuthToken(zToken);
+
+                    }
+                    
                 }
                 else
                 {
